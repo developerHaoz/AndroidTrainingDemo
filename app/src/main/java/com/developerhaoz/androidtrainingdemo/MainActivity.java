7a0a0e30bc1699b70b58dc9f5f6e6a3935b4b889
Haoz <developerhaon@gmail.com>
2017-09-09 21:06:19 +0800

    FileProvider 的一些测试代码

--- app/src/main/java/com/developerhaoz/androidtrainingdemo/MainActivity.java
+++ app/src/main/java/com/developerhaoz/androidtrainingdemo/MainActivity.java
@@ -1,13 +1,86 @@
 package com.developerhaoz.androidtrainingdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_TAKE_PHOTO = 0;
+    private String mCurrentPhotoPath;
+    private ImageView mIvShowPhoto;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mIvShowPhoto = (ImageView) findViewById(R.id.main_iv_show_photo);
+        findViewById(R.id.main_btn_start_activity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getPhotoWithCamera();
+            }
+        });
+
+    }
+
+    private void getPhotoWithCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            String filename = System.currentTimeMillis() + ".png";
+            File file = new File(Environment.getExternalStorageDirectory(), filename);
+            mCurrentPhotoPath = file.getAbsolutePath();
+            Uri fileUri = FileProvider.getUriForFile(MainActivity.this, "com.developerhaoz.androidtrainingdemo.fileprovider", file);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+            startActivityForResult(takePictureIntent, REQUEST_CODE_TAKE_PHOTO);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_TAKE_PHOTO){
+            Log.d(TAG, "onActivityResult: " + mCurrentPhotoPath);
+            Glide.with(this).load(mCurrentPhotoPath).into(mIvShowPhoto);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
