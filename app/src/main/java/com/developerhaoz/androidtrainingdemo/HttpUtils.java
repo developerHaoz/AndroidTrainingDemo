ffc85b8c70a3ddeadae08e26a8d01e16cba54745
Haoz <developerhaon@gmail.com>
2017-10-04 23:05:05 +0800

    一些小 Demo

---
+++ app/src/main/java/com/developerhaoz/androidtrainingdemo/HttpUtils.java
@@ -0,0 +1,46 @@
+package com.developerhaoz.androidtrainingdemo;
+
+/**
+ * 网络请求相关的工具类
+ *
+ * Created by developerHaoz on 2017/10/4.
+ */
+
+public class HttpUtils {
+
+//    public static String downloadUrl(URL url) throws IOException{
+//        InputStream stream = null;
+//        HttpsURLConnection connection = null;
+//        String result = null;
+//
+//        try{
+//            connection = (HttpsURLConnection) url.openConnection();
+//            connection.setReadTimeout(3000);
+//            connection.setConnectTimeout(3000);
+//            connection.setRequestMethod("GET");
+//            connection.setDoInput(true);
+//            connection.connect();
+//
+//
+//        }
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/developerhaoz/androidtrainingdemo/NetworkActivity.java
@@ -0,0 +1,40 @@
+package com.developerhaoz.androidtrainingdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by developerHaoz on 2017/10/4.
+ */
+
+public class NetworkActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_network);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
