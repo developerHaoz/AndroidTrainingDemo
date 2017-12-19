d009296f7d7bff079ef2bb61d411a9567261f062
Haoz <developerhaon@gmail.com>
2017-12-19 21:57:23 +0800

    昨天居然忘了签到，这不科学。

---
+++ app/src/main/java/com/developerhaoz/androidtrainingdemo/download/DownloadActivity.java
@@ -0,0 +1,15 @@
+package com.developerhaoz.androidtrainingdemo.download;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.developerhaoz.androidtrainingdemo.R;
+
+public class DownloadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download);
+    }
+}
---
+++ app/src/main/java/com/developerhaoz/androidtrainingdemo/download/DownloadService.java
@@ -0,0 +1,124 @@
+package com.developerhaoz.androidtrainingdemo.download;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+import com.developerhaoz.androidtrainingdemo.R;
+
+import java.io.File;
+
+public class DownloadService extends Service {
+
+    private DownloadTask mDownloadTask;
+    private String mDownloadUrl;
+
+    private DownloadListener mListener = new DownloadListener() {
+        @Override
+        public void onProgress(int progress) {
+            getNotificationManager().notify(1, getNotification("Downloading...", progress));
+        }
+
+        @Override
+        public void onSuccess() {
+            mDownloadTask = null;
+            stopForeground(true);
+            getNotificationManager().notify(1, getNotification("Download Success", -1));
+            Toast.makeText(DownloadService.this, "Download Success", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onFailed() {
+            mDownloadTask = null;
+            stopForeground(true);
+            getNotificationManager().notify(1, getNotification("Download Failed", -1));
+            Toast.makeText(DownloadService.this, "Download Failed", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPaused() {
+            mDownloadTask = null;
+            Toast.makeText(DownloadService.this, "Paused", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCanceled() {
+            mDownloadTask = null;
+            stopForeground(true);
+            Toast.makeText(DownloadService.this, "Canceled", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
+    public DownloadService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    class DownloadBinder extends Binder {
+        public void startDownload(String url) {
+            if (mDownloadTask == null) {
+                mDownloadUrl = url;
+                mDownloadTask = new DownloadTask(mListener);
+                mDownloadTask.execute(mDownloadUrl);
+                startForeground(1, getNotification("Downloading...", 0));
+                Toast.makeText(DownloadService.this, "Downloading...", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        public void pauseDownload() {
+            if (mDownloadTask != null) {
+                mDownloadTask.pauseDownload();
+            }
+        }
+
+        public void cancelDownload() {
+            if (mDownloadTask != null) {
+                mDownloadTask.cancelDownload();
+            } else {
+                if (mDownloadUrl != null) {
+                    String fileName = mDownloadUrl.substring(mDownloadUrl.lastIndexOf("/"));
+                    String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+                    File file = new File(directory + fileName);
+                    if (file.exists()) {
+                        file.delete();
+                    }
+                    getNotificationManager().cancel(1);
+                    stopForeground(true);
+                    Toast.makeText(DownloadService.this, "Canceled", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private NotificationManager getNotificationManager() {
+        return (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    private Notification getNotification(String title, int progress) {
+        Intent intent = new Intent(this, DownloadActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        builder.setContentTitle(title);
+        builder.setContentIntent(pi);
+        if (progress > 0) {
+            builder.setContentText(progress + "%");
+            builder.setProgress(100, progress, false);
+        }
+        return builder.build();
+    }
+}
