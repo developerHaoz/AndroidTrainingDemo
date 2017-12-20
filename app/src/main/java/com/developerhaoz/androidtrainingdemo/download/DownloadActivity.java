216bcf25f224e4d82687c8052f5545d0a50df742
Haoz <developerhaon@gmail.com>
2017-12-20 12:45:37 +0800

    最近感觉好像迷上了刷 LeetCode，贼有毒。

--- app/src/main/java/com/developerhaoz/androidtrainingdemo/download/DownloadActivity.java
+++ app/src/main/java/com/developerhaoz/androidtrainingdemo/download/DownloadActivity.java
@@ -2,14 +2,23 @@ package com.developerhaoz.androidtrainingdemo.download;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.developerhaoz.androidtrainingdemo.R;
 
 public class DownloadActivity extends AppCompatActivity {
 
+    private Button mBtnStartDownload;
+    private Button mBtnPauseDownload;
+    private Button mBtnCancelDownload;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
+        mBtnStartDownload = (Button) findViewById(R.id.download_start_download);
+        mBtnPauseDownload = (Button) findViewById(R.id.download_pause_download);
+        mBtnCancelDownload = (Button) findViewById(R.id.download_cancel_download);
+
     }
 }
